1542c610c2fc3aa1d5d5382328c4fe3805be8377
rata854 <nitiham2006@gmail.com>
2021-09-30 08:22:37 +0000

    admin/controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :costomers
+  
   devise_for :admins
+  
+  namespace :admin do
+    resources :products, only: [:index,:new,:create,:show,:edit,:update]
+    resources :orders, only: [:index,:show,:update]
+    resources :genres, only: [:index,:create,:edit,:update, :show]
+    resources :costomers, only: [:index,:show,:edit,:update]
+    resources :order_products, only: [:update]
+  end
 
+  devise_for :costomers
 
 end
