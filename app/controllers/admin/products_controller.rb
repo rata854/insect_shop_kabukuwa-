b7ed1fee4c1bb3ff2c622e33da36850bbe44ac5c
rata854 <nitiham2006@gmail.com>
2021-10-01 05:53:41 +0000

    product_form

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,14 +1,23 @@
 class Admin::ProductsController < ApplicationController
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      # flash[:info] = "新商品を登録しました"
+      redirect_to admin_product_path(@product)
+    else
+      render :new
+    end
   end
 
   def index
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
@@ -16,4 +25,11 @@ class Admin::ProductsController < ApplicationController
 
   def update
   end
+
+  private
+
+  def product_params
+    params.permit(:name, :detail, :price, :image, :sales_status, :genre_id)
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,12 @@
 class Product < ApplicationRecord
-  
-  has_many :cart_products, :cart_products, :orders, :order_products
+
+  has_many :cart_products
+  has_many :order_products
   belongs_to :genre
-  
-  attachment :image_id
-  
-  validates :genre_id, :name, :price, presence: true
+
+  validates  :name, :price, presence: true
   validates :price, numericality: { only_integer: true }
-  
+
+  attachment :image
+
 end
