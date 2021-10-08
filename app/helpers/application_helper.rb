1dd805e99c4906e03e3f08ee872cc2f993f930f0
rata854 <nitiham2006@gmail.com>
2021-10-08 02:18:06 +0000

    合計金額メソッド

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  # 消費税計算
+  def tax_included_price(price)
+    (price * 1.1).floor(0)
+  end
+
+  # # 小計計算
+  def sub_price(product_price, product_amount)
+    (tax_included_price(product_price)) * (product_amount)
+  end
+
+  def total_price(cart_products)
+    price = 0
+    cart_products.each do |cart_product|
+      product = Product.find(cart_product.product_id)
+  	  price += (cart_product.amount * (product.price * 1.1).round)
+  	end
+    return price
+  end
+
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  # 消費税計算
-  def tax_included_price
-    (self.price * 1.1).floor(0)
-  end
-
 end
