8ef58093ea9938723074d21a34eebe32a6c9db29
rata854 <nitiham2006@gmail.com>
2021-10-09 07:42:29 +0000

    請求画面

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,13 @@
 class Order < ApplicationRecord
-  
+
   has_many :order_products
   belongs_to :current
-  
-  validates :customer_id, :address, :name, :products,
+
+  validates :customer_id, :address, :name, :postage,
             :total_price, :payment_method, presence: true
   validates :postal_code, length: {is: 7}, numericality: { only_integer: true }
-  
+
   enum payment_method: { "クレジットカード": 0, "銀行振込": 1 }
   enum order_status: { "入金待ち": 0, "入金確認": 1, "発送準備": 2, "発送済み": 3 }
-  
+
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -2,7 +2,7 @@ class ShippingAddress < ApplicationRecord
   
   belongs_to :customer
   
-  validetes :customer_id, :name, :address, presence: true
+  # validetes :customer_id, :name, :address, presence: true
   validates :postal_code, length: {is: 7}, numericality: { only_integer: true }
   
 end
